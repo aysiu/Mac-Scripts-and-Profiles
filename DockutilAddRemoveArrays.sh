d954c40ac987f71bbf4da34326f194ab98bfbec8
A.Y. Siu <aysiu@users.noreply.github.com>
2016-04-28 13:00:30 -0700

    Create DockutilAddRemoveArrays.sh

---
+++ DockutilAddRemoveArrays.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## This allows you to specify lists of items to remove and add in arrays, and then they'll be done in bulk using a for loop
+## Items to remove should be the label (usually the name of the application)
+## Items to add are the full path to the item to add (usually /Applications/NAMEOFAPP.app)
+## A few examples are prepopulated in the arrays, but feel free to tweak as suits the needs of your organization
+
+itemsToRemove=(
+   "App Store"
+   "System Preferences"
+)
+
+itemsToAdd=(
+   "/Applications/TextWrangler.app"
+   "/Applications/Firefox.app"
+)
+
+for removalItem in "${itemsToRemove[@]}"
+   do
+      # Check that the item is actually in the Dock
+      inDock=$(/usr/local/bin/dockutil --list | grep "$removalItem")
+      if [ ! -z "$inDock" ]; then
+         /usr/local/bin/dockutil --remove "$removalItem" --no-restart
+      fi
+   done
+
+
+for additionItem in "${itemsToAdd[@]}"
+   do
+      # Check that the item is actually exists to be added to the Dock and that it isn't already in the Dock
+      # Stripping path and extension code based on code from http://stackoverflow.com/a/2664746
+      additionItemString=${additionItem##*/}
+      additionItemBasename=${additionItemString%.*}
+      inDock=$(/usr/local/bin/dockutil --list | grep "additionItemBasename")
+      if [ -e "$additionItem" ] && [ -z "$inDock" ]; then
+            /usr/local/bin/dockutil --add "$additionItem" --no-restart
+      fi
+   done
+
+killall Dock
