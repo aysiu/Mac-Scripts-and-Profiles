892852e8e7f6bc3b0baf339aeb553e278ebc3bc8
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-02 09:26:52 -0700

    Delete TextWranglerSuppressUpgradePrompt.sh

--- TextWranglerSuppressUpgradePrompt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get current date and time
-currentDate=$(date +"%Y-%m-%dT%H:%M:%SZ")
-
-# Write it back to the .plist
-sudo /usr/bin/defaults write /Library/Preferences/com.barebones.textwrangler LastUpgradeAlertDate -date "$currentDate"
