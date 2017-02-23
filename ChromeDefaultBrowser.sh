b510bf4342afcf776d36824ff389f91066a32cc9
aysiu <aysiu@users.noreply.github.com>
2017-02-23 09:10:28 -0800

    Major cleanup
    Specified path and alternate path to lsregister
    Included test for lsregister path existence (with warning)
    Added in logic for special case scenario for public.url
    Simplified while loop by removing test for number of changes made

--- ChromeDefaultBrowser.sh
+++ ChromeDefaultBrowser.sh
@@ -10,8 +10,13 @@ DefaultBrowser='com.google.chrome'
 # PlistBuddy executable
 PlistBuddy='/usr/libexec/PlistBuddy'
 
-# Plist Location
+# Plist location
 PlistLocation="$HOME/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist"
+
+# lsregister location (this location appears to exist on most macOS systems)
+lsregister='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister'
+# This is an alternate location, but I think anything with the location below should also have the location above
+#lsregister='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister'
 
 ## Things to change
 #### LSHandlerContentType = public.url
@@ -27,44 +32,48 @@ PlistLocation="$HOME/Library/Preferences/com.apple.LaunchServices/com.apple.laun
 if [ -f "$PlistLocation" ]; then
 
    # Initialize counter that will just keep moving us through the array of dicts
-   # A bit imprecise... would be better if we could just count the array of dicts
+   # A bit imprecise... would be better if we could just count the array of dicts, but we'll stop when we get to a blank one
    Counter=0
 
-   # Initialize test variable
-   PrefsChanged=0
+   # Initialize DictResult just so the while loop begins
+   DictResult='PLACEHOLDER'
 
-   while [ "$PrefsChanged" -lt 4 ]; do
+   while [[ ! -z "$DictResult" ]]; do
       DictResult=$("$PlistBuddy" -c "Print LSHandlers:$Counter" "$PlistLocation")
-      
-      # The counter has gone too high, so just stop the while loop
-      if [[ -z "$DictResult" ]]; then
-         PrefsChanged=4
 
-      elif [[ "$DictResult" == *"public.url"* ]]; then
+      if [[ "$DictResult" == *"public.url"* ]]; then
          echo "Changing public.url. Counter is $Counter"
-   
-         "$PlistBuddy" -c "Set :LSHandlers:$Counter:LSHandlerRoleViewer $DefaultBrowser" "$PlistLocation"
-   
-         PrefsChanged=$((PrefsChanged+1))
-   
-      elif [[ "$DictResult" == *"public.html"* ]] || [[ "$DictResult" == *"https"* ]] || [[ "$DictResult" == *"http"* ]]; then
+
+         # Apparently, sometimes it's LSHandlerRoleViewer and other times LSHandlerRoleAll, so let's check...
+         if [[ "$DictResult" == *"LSHandlerRoleViewer"* ]]; then
+            "$PlistBuddy" -c "Set :LSHandlers:$Counter:LSHandlerRoleViewer $DefaultBrowser" "$PlistLocation"
+         elif [[ "$DictResult" == *"LSHandlerRoleAll"* ]]; then
+            "$PlistBuddy" -c "Set :LSHandlers:$Counter:LSHandlerRoleAll $DefaultBrowser" "$PlistLocation"            
+         fi
+
+      elif [[ "$DictResult" == *"public.html"* ]] || [[ "$DictResult" == *"LSHandlerURLScheme = https"* ]] || [[ "$DictResult" == *"LSHandlerURLScheme = http"* ]]; then
 
          echo "Changing public.html or https or http. Counter is $Counter"
-   
+
          "$PlistBuddy" -c "Set :LSHandlers:$Counter:LSHandlerRoleAll $DefaultBrowser" "$PlistLocation"
 
-         PrefsChanged=$((PrefsChanged+1))
-   
       fi
 
       # Increase counter
      Counter=$((Counter+1))
 
+   # End of while loop
    done
 
-   echo "Rebuilding Launch services. This may take a few moments."
-   # Rebuilding launch services
-   /System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain system -domain user
+   # Check the lsregister location exists
+   if [ -f "$lsregister" ]; then
+
+      echo "Rebuilding Launch services. This may take a few moments."
+      # Rebuilding launch services
+      "$lsregister" -kill -r -domain local -domain system -domain user
+   else
+      echo "You may need to log out or reboot for changes to take effect. Cannot find location of lsregister at $lsregister"
+   fi
 
 # Plist does not exist
 else
