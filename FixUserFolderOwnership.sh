20b9c493020f1d5cdb0309d37180938ce87d6e94
aysiu <aysiu@users.noreply.github.com>
2017-03-08 09:51:34 -0800

    Better way to get user home directories
    Home directory not always the same as username

--- FixUserFolderOwnership.sh
+++ FixUserFolderOwnership.sh
@@ -3,18 +3,18 @@
 ## Just in case you're copying folders over as root and the ownership of folders changes, this script will change the ownership back,
 ## assuming that the short name is the same as the user folder name, which it usually is
 
-# Change to the /Users directory
-cd /Users
+# Get list of active users
+# From https://www.jamf.com/jamf-nation/discussions/3736/dscl-command-to-list-local-users-but-exclude-system-accounts#responseChild17416
+USER_LIST=$(dscl /Local/Default -list /Users uid | awk '$2 >= 100 && $0 !~ /^_/ { print $1 }')
 
-# Loop through each user
-for user in *; do
+# Loop through list of users
+for THIS_USER in $USER_LIST; do
 
-  # Make an exception for the Shared folder
-  if [ "$user" != "Shared" ]; then
+   # Determine the user home directory
+   USER_HOME=$(dscl . -read "/Users/$THIS_USER" NFSHomeDirectory | awk '{print $2}')
 
-    # Change ownership
-    chown -R "$user" /Users/"$user"
+   # Change ownership back to the original owner
+   sudo chown -R "$THIS_USER" "$USER_HOME"
 
-  fi
-
+# End looping through list of users
 done
