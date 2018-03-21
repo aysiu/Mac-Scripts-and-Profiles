020c8fc550d35e1275b3699bb2844921dc2fec01
aysiu <aysiu@users.noreply.github.com>
2018-03-21 11:44:06 -0700

    Full paths to binaries

--- ClearLocalUserFolders/preinstall_script_munki.sh
+++ ClearLocalUserFolders/preinstall_script_munki.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Make sure not user is logged in
-lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow lastUser)
+lastUser=$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindow lastUser)
 
 if [ "$lastUser" != "loggedIn" ]; then
 
    # Exit cleanly, we can proceed
-   #echo "User not logged in"
+   # /bin/echo "User not logged in"
    exit 0
 
 else
 
    # Exit 1, so we'll have to re-run this again
-   #echo "User is logged in"
+   # /bin/echo "User is logged in"
    exit 1
 
 fi
