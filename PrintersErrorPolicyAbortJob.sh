b433b8b687b80abe4517aa4afad956ad722b82df
aysiu <aysiu@users.noreply.github.com>
2018-03-21 12:05:01 -0700

    Full paths to binaries

--- PrintersErrorPolicyAbortJob.sh
+++ PrintersErrorPolicyAbortJob.sh
@@ -3,11 +3,11 @@
 # Changes all printers' error policies to be abort-job instead of the default (stop-printer)
 
 # Loop through all the installed printers
-lpstat -s | awk -F "device for " '{print $2}' | awk -F ":" '{print $1}' | while read line; do
+/usr/bin/lpstat -s | /usr/bin/awk -F "device for " '{print $2}' | /usr/bin/awk -F ":" '{print $1}' | while read line; do
    # If the line isn't empty (there may be one empty line at the top)..
    if [ ! -z "$line" ]; then
       # Change the error policy to be abort-job
-      echo "Changing error policy to abort job for $line"
+      /bin/echo "Changing error policy to abort job for $line"
       /usr/sbin/lpadmin -p "$line" -o printer-error-policy=abort-job
    fi
 done
