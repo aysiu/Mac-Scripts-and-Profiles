ab494d054d4f45e86abe5753bcfbc98f3fd58bf7
aysiu <aysiu@users.noreply.github.com>
2019-05-21 08:56:21 -0700

    Fixed if/then logic and check_output
    Before, even if there was verification failure (couldn't even verify—not that the verification had a mismatch), the script would still prompt to trust changes. Now it will skip all that if there's a verification failure. 
    
    Also added in an `stderr=subprocess.STDOUT` parameter to `check_output`, because it was failing on the AutoPkg 1.1 release candidate.

--- AutoPkgReviewAndRun.py
+++ AutoPkgReviewAndRun.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 
-### This script updates AutoPkg repos, verifies trust info (with prompt to update after review), and runs recipes in a recipe list
-
 import os
 import subprocess
 
 # Where is the recipe list (one recipe per line) located?
-# Recipe list should be one recipe per line, separated by a carriage return ("\n")
-recipe_list='/PATH/TO/recipe_list.txt'
+recipe_list='/Users/admin/Library/Application Support/AutoPkgr/recipe_list.txt'
 
 # Acceptable affirmative responses
 affirmative_responses=["y", "yes", "sure", "definitely"]
@@ -16,7 +13,7 @@ def main():
     # Double-check the recipe list file exists
     if os.path.exists(recipe_list):
         # Update the repos
-        subprocess.call(["/usr/local/bin/autopkg","repo-update","all"])
+        subprocess.call(["autopkg","repo-update","all"])
         # Create an empty dictionary of recipes that need to be verified
         # Put the recipes into a list
         recipes = [recipe.rstrip('\n') for recipe in open(recipe_list)]
@@ -25,21 +22,24 @@ def main():
             print "Verifying trust info for %s" % recipe
             # See what the verified trust info looks like
             try:
-                verify_result=subprocess.check_output(["/usr/local/bin/autopkg", "verify-trust-info", "-vv", recipe])
+                verify_result=subprocess.check_output(["autopkg", "verify-trust-info", "-vv", recipe], stderr=subprocess.STDOUT)
             except:
                 verify_result="Verification failure"
-            desired_result=recipe + ": OK"
-            if desired_result not in verify_result:
-                confirmation=raw_input("Do you trust these changes? (y/n) ")
-                if confirmation.lower().strip() in affirmative_responses:
-                    print "Updating trust info for %s" % recipe
-                    subprocess.call(["/usr/local/bin/autopkg", "update-trust-info", recipe])
-                else:
-                    print "Okay. Not updating trust for %s" % recipe
-                    # Remove it from the list of recipes to run... no point in running it if the trust info isn't good
-                    recipes.remove(recipe)
+            if verify_result=="Verification failure":
+                print "Unable to verify %s" % recipe
+            else:
+                desired_result=recipe + ": OK"
+                if desired_result not in verify_result:
+                    confirmation=raw_input("Do you trust these changes? (y/n) ")
+                    if confirmation.lower().strip() in affirmative_responses:
+                        print "Updating trust info for %s" % recipe
+                        subprocess.call(["autopkg", "update-trust-info", recipe])
+                    else:
+                        print "Okay. Not updating trust for %s" % recipe
+                        # Remove it from the list of recipes to run... no point in running it if the trust info isn't good
+                        recipes.remove(recipe)
         # Whether there were things to verify or not, go ahead and run the recipes
-        cmd = [ "/usr/local/bin/autopkg", "run" ]
+        cmd = [ "autopkg", "run" ]
         cmd.extend(recipes)
         subprocess.call(cmd)
 
